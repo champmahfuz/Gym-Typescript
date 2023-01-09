ea0c8d555dd73b04653f37b5f72154d5611e71f8
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-01-10 03:03:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
+import { SelectedPage } from "./shared/types";
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
+}
+
+export default App;
